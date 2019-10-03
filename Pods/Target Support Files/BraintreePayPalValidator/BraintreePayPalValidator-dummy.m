c2547fcbe298bf6b3a95497130af4bdf55e72012
scannillo <samantha.cannillo@getbraintree.com>
2019-10-03 16:48:36 -0500

    Add Pods & docs image asset
    Co-authored-by: scannillo <sammyjaynecannillo@gmail.com>
    Co-authored-by: dmerino <david.eros.merino@gmail.com>

---
+++ Pods/Target Support Files/BraintreePayPalValidator/BraintreePayPalValidator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BraintreePayPalValidator : NSObject
+@end
+@implementation PodsDummy_BraintreePayPalValidator
+@end
---
+++ Pods/Target Support Files/BraintreePayPalValidator/BraintreePayPalValidator-umbrella.h
@@ -0,0 +1,19 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BraintreePayPalValidator.h"
+#import "BTPayPalValidateResult.h"
+#import "BTPayPalValidatorClient.h"
+
+FOUNDATION_EXPORT double BraintreePayPalValidatorVersionNumber;
+FOUNDATION_EXPORT const unsigned char BraintreePayPalValidatorVersionString[];
+
