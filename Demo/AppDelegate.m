7912d7474e89a95018dc167322295c26fe344cad
scannillo <samantha.cannillo@getbraintree.com>
2019-10-15 10:56:42 -0500

    Fix url scheme validation comparison. Was never passing, due to missing call to lowercase the scheme string.

--- Demo/AppDelegate.m
+++ Demo/AppDelegate.m
@@ -1,23 +1,19 @@
 #import "AppDelegate.h"
 #import "Demo-Swift.h"
 
-@interface AppDelegate ()
-
-// TODO: Make constant for PaymentsURLScheme, @"com.braintreepayments.Demo.payments"
-
-@end
+NSString *BraintreeDemoAppDelegatePaymentsURLScheme = @"com.braintreepayments.Demo.payments";
 
 @implementation AppDelegate
 
 - (BOOL)application:(__unused UIApplication *)application didFinishLaunchingWithOptions:(__unused NSDictionary *)launchOptions {
-    [BTAppSwitch setReturnURLScheme:@"com.braintreepayments.Demo.payments"];
+    [BTAppSwitch setReturnURLScheme: BraintreeDemoAppDelegatePaymentsURLScheme];
 
     return YES;
 }
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
 
-    if ([url.scheme.lowercaseString  isEqualToString:@"com.braintreepayments.Demo.payments"]) {
+    if ([[url.scheme lowercaseString] isEqualToString:[BraintreeDemoAppDelegatePaymentsURLScheme lowercaseString]]) {
         return [BTAppSwitch handleOpenURL:url options:options];
     }
 
